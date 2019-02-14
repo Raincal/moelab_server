6e106ddb7b395ade148d6a694407ff89be589441
Raincal <cyj94228@gmail.com>
2019-02-14 23:36:59 +0800

    :recycle: (schema) add role to user_input

--- lib/moelab_server_web/schema/account/account_types.ex
+++ lib/moelab_server_web/schema/account/account_types.ex
@@ -14,6 +14,7 @@ defmodule MoelabServerWeb.Schema.Account.AccountTypes do
     field(:name, non_null(:string))
     field(:username, non_null(:string))
     field(:email, non_null(:string))
+    field(:role, :string)
     field(:password, non_null(:string))
     field(:password_confirmation, non_null(:string))
   end
@@ -21,11 +22,6 @@ defmodule MoelabServerWeb.Schema.Account.AccountTypes do
   object :session do
     field(:token, :string)
     field(:user, :user)
-  end
-
-  input_object :session_input do
-    field(:email, non_null(:string))
-    field(:password, non_null(:string))
   end
 
   object :register_result do
